77b6fa32084e3d7f163dae6ae4837dbaa3b6a068
rmkns <113551004+rmkns@users.noreply.github.com>
2022-09-14 21:54:55 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("     .---.\n");
+    printf("    @ @   )\n");
+    printf("    ^     |\n");
+    printf("   [|]    | ##\n");
+    printf("   /      |####\n");
+    printf("  (       |#### \n");
+    printf("   \\| /   |#BP#\n");
+    printf("  / |.'   |### \n");
+    printf(" _\\ ``\\   )##\n");
+    printf("/,,_/,,____#\n");
+    return 0;
+}
+
