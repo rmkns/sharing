2982dd0154f31a6880ea77d14608cf809ec75e21
rmkns <113551004+rmkns@users.noreply.github.com>
2022-09-14 21:55:48 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,18 +1,17 @@
-#include <stdio.h>
+#include <stdio.h>    //
 #include <stdlib.h>
 
 int main()
 {
-    printf("     .---.\n");
-    printf("    @ @   )\n");
-    printf("    ^     |\n");
-    printf("   [|]    | ##\n");
-    printf("   /      |####\n");
-    printf("  (       |#### \n");
-    printf("   \\| /   |#BP#\n");
-    printf("  / |.'   |### \n");
-    printf(" _\\ ``\\   )##\n");
-    printf("/,,_/,,____#\n");
+
+    char characterName[] = "Jon"; // %s
+    int characterAge = 91;         // %d
+    printf("There once was a man named %s\n", characterName);
+    printf("he was %d years old.\n", characterAge);
+
+    characterAge = 30;
+    printf("He really like the name %s\n", characterName);
+    printf("but he did not like being %d.\n", characterAge);
+
     return 0;
 }
-
