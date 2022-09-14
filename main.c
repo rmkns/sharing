6e32bf641cd2bbf8951754ad80bd2f41a0cd7978
rmkns <113551004+rmkns@users.noreply.github.com>
2022-09-14 22:06:04 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double skaiciusx, skaiciusz, skaiciusy;
+     printf("Iveskite 3 skaicius:\n");
+    scanf("%lf %lf %lf", &skaiciusx,&skaiciusy,&skaiciusz);
+    return 0;
+}
